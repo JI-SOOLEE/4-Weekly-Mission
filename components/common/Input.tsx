dbe976a7070facab99adb77f5aa95f343f6ef0d0
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-03-31 23:37:51 +0900

    Feat: 비밀번호 eye on/off 기능 추가

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -1,15 +1,31 @@
-import { useForm } from 'react-hook-form';
-
+import Image from 'next/image';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import eyeOn from '@/public/assets/eye-on.svg';
+import eyeOff from '@/public/assets/eye-off.svg';
 interface FormValue {
   email: string;
   password: string;
 }
 
 const Input = () => {
-  const { register, handleSubmit } = useForm<FormValue>();
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>();
 
   const onSubmit = (data: FormValue) => {
     console.log(data);
+  };
+
+  const [inputType, setInputType] = useState('password');
+
+  const handleInputType = () => {
+    setInputType((currentInputType) =>
+      currentInputType === 'password' ? 'text' : 'password'
+    );
   };
 
   return (
@@ -17,11 +33,24 @@ const Input = () => {
       <label>이메일</label>
       <input
         type='email'
-        {...register('email', { required: true })}
-        placeholder='내용 입력'
+        {...register('email', {
+          required: '이메일을 입력해주세요.',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: '이메일 형식에 맞지 않습니다.',
+          },
+        })}
+        placeholder='이메일을 입력해주세요.'
       />
+      {errors.email?.type === 'pattern' && <p>{errors.email.message}</p>}
+
       <label>비밀번호</label>
-      <input type='password' {...register('password', { required: true })} />
+      <input type={inputType} {...register('password', { required: true })} />
+      <Image
+        onClick={handleInputType}
+        src={inputType === 'password' ? eyeOff : eyeOn}
+        alt='Toggle Password Visibility'
+      />
       <button type='submit'>로그인</button>
     </form>
   );
