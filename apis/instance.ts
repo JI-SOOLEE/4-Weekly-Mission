643adbd84c35e811bf00ecd38274c44b105779e6
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-08 15:18:01 +0900

    axios 설치 & instance.ts 생성

---
+++ apis/instance.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const baseUrl = process.env.NEXT_PUBLIC_API_KEY;
+
+export const instance = axios.create({
+  baseURL: baseUrl,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
